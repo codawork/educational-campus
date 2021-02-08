e35c49ca41794f2848732fb99b60db1d9a0d50b6
SantiagoBobrik <santiago.bobrik@gmail.com>
2021-02-07 21:25:24 -0300

    add UserSchema

---
+++ config/Models/User.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require("mongoose");
+const validator = require("validator");
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain the word "password"');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+
+  avatar: {
+    type: Buffer,
+  },
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to log in");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to log in");
+  }
+
+  return user;
+};
+
+userSchema.statics.findByEmail = async (email) => {
+  const user = await User.findOne({ email });
+  return user;
+};
+module.exports = model("User", userSchema);
